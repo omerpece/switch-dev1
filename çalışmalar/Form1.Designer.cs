db61a3757e415b8dd14326fb29c28938523027f7
omerpece <pece.omer@outlook.com>
2019-07-17 16:27:54 +0300

    switch ödevi

---
+++ çalışmalar/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace çalışmalar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.txtgiris1 = new System.Windows.Forms.TextBox();
+            this.txtgiris2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(7, 64);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(166, 23);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "SORGU";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // txtgiris1
+            // 
+            this.txtgiris1.Location = new System.Drawing.Point(12, 12);
+            this.txtgiris1.Name = "txtgiris1";
+            this.txtgiris1.Size = new System.Drawing.Size(166, 20);
+            this.txtgiris1.TabIndex = 1;
+            // 
+            // txtgiris2
+            // 
+            this.txtgiris2.Location = new System.Drawing.Point(12, 38);
+            this.txtgiris2.Name = "txtgiris2";
+            this.txtgiris2.Size = new System.Drawing.Size(166, 20);
+            this.txtgiris2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(185, 161);
+            this.Controls.Add(this.txtgiris2);
+            this.Controls.Add(this.txtgiris1);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.TextBox txtgiris1;
+        private System.Windows.Forms.TextBox txtgiris2;
+    }
+}
+
---
+++ çalışmalar/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace çalışmalar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //İç içe switch kullanarak kullanıcı adını ve şifresini kontrol ediniz.eğer kullanıcı kullanıcıadını ve şifresini doğru girerse "Tebrikler, hem kullanıcı adı hemde şifreniz doğru" uyarısı
+            //Kullanıcı adı doğru şifre yanlışsa "Kullanıcı adınız doğru ancak şifreniz yanlış!" uyarısı
+            //kullanıcı adı yanlışsa "Kullanıcı adınızı yanlış girdiniz, şifreye bakmadım bile!" uyarusını kullanıcıya veriniz.
+
+            //KullaniciAdi = "bilgeadam";
+            //Sifre = "1234";
+
+            string metin = txtgiris1.Text.ToLower();
+            int sayi = int.Parse(txtgiris2.Text);
+
+            switch (metin)
+            {
+                case "bilgeadam":
+                    switch(sayi)
+                    {
+                        case 1234:
+                            MessageBox.Show("kullanıcı adı ve şifre doğru");
+                            break;
+                        default:
+                            MessageBox.Show("kullanıcı adı doğru ancak şifreniz hatalı");
+                            break;
+                    }
+                    break;
+                default:
+                    MessageBox.Show("kullanıcı adınız hatalıdır");
+                    break;
+            }
+
+        }
+    }
+}
